7a06df618725911baf477098c445788eaae39edb
mark joseph <markjoseph.dagami@gmail.com>
2017-01-18 16:09:26 +0800

    updated companyInfo controller

---
+++ app/Http/Controllers/CompanyInfoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\CompanyInfoModel;
+use App\SignupModel;
+use DB;
+
+class CompanyInfoController extends Controller
+{
+    public function store(Request $request)
+    {
+    	
+
+    }
+}
